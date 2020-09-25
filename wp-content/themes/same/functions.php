c7398366f185ed2494b853f7b28aa0cafd19b869
azuborev <azuborev@corp.web4pro.com.ua>
2020-09-25 17:44:02 +0300

    s

--- wp-content/themes/same/functions.php
+++ wp-content/themes/same/functions.php
@@ -5,108 +5,51 @@
  */
 
 if ( ! defined( '_S_VERSION' ) ) {
-	// Replace the version number of the theme on each release.
 	define( '_S_VERSION', '1.0.0' );
 }
 
 if ( ! function_exists( 'same_setup' ) ) :
-	/**
-	 * Sets up theme defaults and registers support for various WordPress features.
-	 *
-	 * Note that this function is hooked into the after_setup_theme hook, which
-	 * runs before the init hook. The init hook is too late for some features, such
-	 * as indicating support for post thumbnails.
-	 */
+
 	function same_setup() {
-		/*
-		 * Make theme available for translation.
-		 * Translations can be filed in the /languages/ directory.
-		 * If you're building a theme based on same, use a find and replace
-		 * to change 'same' to the name of your theme in all the template files.
-		 */
-		load_theme_textdomain( 'same', get_template_directory() . '/languages' );
+        load_theme_textdomain('same', get_template_directory() . '/languages');
+        add_theme_support('post-thumbnails');
+        register_nav_menus(
+            [
+                'menu-1' => esc_html__('Primary', 'same'),
+            ]
+        );
 
-		/*
-		 * Enable support for Post Thumbnails on posts and pages.
-		 *
-		 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
-		 */
-		add_theme_support( 'post-thumbnails' );
+        add_theme_support(
+            'html5',
+            [
+                'search-form',
+                'comment-form',
+                'comment-list',
+                'gallery',
+                'caption',
+                'style',
+                'script',
+            ]
+        );
 
-		// This theme uses wp_nav_menu() in one location.
-		register_nav_menus(
-			    [
-				'menu-1' => esc_html__( 'Primary', 'same' ),
-			]
-		);
+        add_theme_support(
+            'custom-background',
+            apply_filters(
+                'same_custom_background_args',
+                [
+                    'default-color' => 'ffffff',
+                    'default-image' => '',
+                ]
+            )
+        );
 
-		/*
-		 * Switch default core markup for search form, comment form, and comments
-		 * to output valid HTML5.
-		 */
-		add_theme_support(
-			'html5',
-			[
-				'search-form',
-				'comment-form',
-				'comment-list',
-				'gallery',
-				'caption',
-				'style',
-				'script',
-			]
-		);
+        add_theme_support('custom-logo');
+    }
 
-		// Set up the WordPress core custom background feature.
-		add_theme_support(
-			'custom-background',
-			apply_filters(
-				'same_custom_background_args',
-				[
-					'default-color' => 'ffffff',
-					'default-image' => '',
-				]
-			)
-		);
-
-		// Add theme support for selective refresh for widgets.
-		add_theme_support( 'customize-selective-refresh-widgets' );
-
-		/**
-		 * Add support for core custom logo.
-		 *
-		 * @link https://codex.wordpress.org/Theme_Logo
-		 */
-		add_theme_support(
-			'custom-logo',
-			array(
-				'height'      => 250,
-				'width'       => 250,
-				'flex-width'  => true,
-				'flex-height' => true,
-			)
-		);
-	}
 endif;
+
 add_action( 'after_setup_theme', 'same_setup' );
 
-/**
- * Set the content width in pixels, based on the theme's design and stylesheet.
- *
- * Priority 0 to make it available to lower priority callbacks.
- *
- * @global int $content_width
- */
-function same_content_width() {
-	$GLOBALS['content_width'] = apply_filters( 'same_content_width', 640 );
-}
-add_action( 'after_setup_theme', 'same_content_width', 0 );
-
-/**
- * Register widget area.
- *
- * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
- */
 function same_widgets_init() {
 	register_sidebar(
 		[
@@ -122,9 +65,6 @@ function same_widgets_init() {
 }
 add_action( 'widgets_init', 'same_widgets_init' );
 
-/**
- * Enqueue scripts and styles.
- */
 function same_scripts() {
 	wp_enqueue_style( 'same-style', get_stylesheet_uri(), [], _S_VERSION );
     wp_enqueue_style( 'same-light_style', get_template_directory_uri().'/assets/css/light.css', [], _S_VERSION );
